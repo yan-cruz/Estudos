24134234f8203cfab078aca7d6441d78f26c1994
Yan Cruz <yancruzmg@gmail.com>
2025-04-06 22:11:46 -0300

    input data

--- Facebook-Dash/graph_app.py
+++ Facebook-Dash/graph_app.py
@@ -19,22 +19,16 @@ def fazer_request(url, params=None):
 dotenv.load_dotenv()
 token = os.environ['ACCESS_TOKEN']
 
-# URL da API
 url = "https://graph.facebook.com/v22.0/me"
 
-# Solicitar as datas ao usuário
-'''since_date = input("Digite a data de início (YYYY-MM-DD): ")
-until_date = input("Digite a data de término (YYYY-MM-DD): ")'''
+since_date = input("Digite a data de início (YYYY-MM-DD): ")
+until_date = input("Digite a data de término (YYYY-MM-DD): ")
 
-since_date = "2025-04-01"
-until_date = "2025-04-05"
 
-# Validar se as datas foram fornecidas
 if not since_date or not until_date:
     print("Erro: As datas de início e término são obrigatórias.")
     exit()
 
-# Parâmetros da requisição
 fields = (
     f"adaccounts.limit(100){{"
     f"name,"
@@ -49,17 +43,14 @@ params = {
     "access_token": token
 }
 
-# Fazer a requisição GET
 response = fazer_request(url, params=params)
 
-# Lista para armazenar todos os clientes processados
 clientes = []
 
 for account in response.get('adaccounts', {}).get('data', []):
     cliente = {}
     cliente['nome'] = account.get('name', 'Desconhecido')
     
-    # Verificar se há insights disponíveis
     insights_data = account.get('insights', {}).get('data', [])
     if insights_data:
         # Investimento
@@ -95,9 +86,7 @@ for account in response.get('adaccounts', {}).get('data', []):
         cliente['compras'] = 0
         cliente['adicoes_carrinho'] = 0
     
-    # Adiciona na lista
     clientes.append(cliente)
 
-# Agora você tem uma lista de dicionários limpa e fácil de filtrar:
 for c in clientes:
     print(c)
